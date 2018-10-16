8246789a2ba623b788a58ae324a73ffab1e79054
Nasr-Azadani, Mohamad <mohamad.nasr-azadani@accenture.com>
2018-10-15 22:50:18 -0700

    Updated hash tables

--- GeeksForGeeks/20_hash_stuff.cpp
+++ GeeksForGeeks/20_hash_stuff.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <map>
 #include <vector>
+#include <set>
+#include <string>
 
 bool isSubset(std::vector<int> vsub, std::vector<int> v) {
 /* check if an array is a subset of another array */
@@ -40,33 +42,84 @@ int countSumPair(std::vector<int> v, int sum) {
 			twice_count--;
 		}
 	}
-	std::cout << "here: " << twice_count << std::endl;
 	return twice_count / 2;
 }
 
 bool hasDuplicatesInKDistance(std::vector<int> v, int k) {
-	std::map<int, int> m;
+	/* check if array has duplicates within a distance k */
+	std::set<int> m;
 	int idx = 0;
-	for (std::vecttor<int>::iterator it=v.begin(); it != v.end(); it++) {
-		if (m[*it].count()) 
+	for (std::vector<int>::iterator it=v.begin(); it != v.end(); it++) {
+		if (m.find(*it) != m.end()) 
 			return true;
-		m[*it] = 1;
-		if (idx >= k) > 
-			m[*it]
-
+		m.insert(*it);
+		if (idx >= k) 
+			m.erase(it[idx-k]);
+		idx++;
 	}
+	return false;
+}
+
+void printTravelItinerary(std::map<std::string, std::string> data) {
+	std::map<std::string, std::string> toFrom;
+	std::map<std::string, std::string>::iterator it;
+	for (it=data.begin(); it != data.end(); it++) {
+		/* key */
+		std::string fromCity = it->first;
+		/* value */
+		std::string toCity = it->second;
+		toFrom[toCity] = fromCity;
+	} /* for */
+
+	std::string startCity;
+	for (it=data.begin(); it != data.end(); it++) {
+		/* found city of origin, since it does not appear in the 'to' list of keys */
+		if (toFrom.find(it->first) == toFrom.end() ) { 
+			startCity = it->first;
+			break;
+		}
+	} /* for */
+	std::cout << "Start city found:" << startCity << std::endl;
+
+	std::string fromCity = startCity;
+	while (data.find(fromCity) != data.end()) {
+		std::string toCity = data[fromCity];
+		std::cout << "From: " << fromCity << " " << "To: " << toCity << std::endl;
+		fromCity = toCity;
+	}
+}
+
+void printEmployeeManagerCount(std::map<char, char> data) {
+	/* data[employee] = manager */
+	std::map<char, std::vector<char>> reverseMap;
+	std::map<char, char>::iterator it;
+
+	for (it = data.begin(); it != data.end(); it++) {
+		char employee = it->first; 
+		char manager = it->second;
+		reverseMap[manager].push_back(employee);
+	}
+
 }
 
 int main(int argc, char* argv[]) {
 	std::vector<int> v1, v2;
-	v1.push_back(0);
 	v1.push_back(1);
+	v1.push_back(0);
 	v1.push_back(2);
-	v1.push_back(3);
+	v1.push_back(1);
 	v2.push_back(0);
 	//std::cout << isSubset(v2, v1) << std::endl;
 	std::cout << findSumPair(v1, 0) << std::endl;
 	std::cout << countSumPair(v1, 3) << std::endl;
+	std::cout << "has duplicates: " << hasDuplicatesInKDistance(v1, 3) << std::endl;
+	std::map<std::string, std::string> fromToCity;
+
+	fromToCity["Isfahan"] = "Tehran";
+	fromToCity["Tehran"] = "Shiraz";
+	fromToCity["Shiraz"] = "Mashhad";
+
+	printTravelItinerary(fromToCity);
 
 	return 0;
 }
